eca5045facb4d3b2a52773a76d41ff66185a7b0e
MeloMike23 <michael.melohn23@gmail.com>
2021-03-04 10:37:55 -0600

    Made the remote-install more efficient

--- remote-install.sh
+++ remote-install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/MeloMike23/dotfiles/main/.vimrc >.vimrc 2>/dev/null
-
-curl https://raw.githubusercontent.com/MeloMike23/dotfiles/main/.inputrc >.inputrc 2>/dev/null
+DOTFILES=(.vimrc .inputrc)
+for f in ${DOTFILES[@]}; do
+    curl https://raw.githubusercontent.com/MeloMike23/dotfiles/main/${f} >~/${f} 2>/dev/null
+done
