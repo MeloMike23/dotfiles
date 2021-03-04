1ad4a670ba38f5d7defa3cff1d88a7f4801a8306
MeloMike23 <michael.melohn23@gmail.com>
2021-03-04 10:21:17 -0600

    Added remote-install script

---
+++ remote-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl https://raw.githubusercontent.com/MeloMike23/dotfiles/main/.vimrc >.vimrc 2>/dev/null
+
+curl https://raw.githubusercontent.com/MeloMike23/dotfiles/main/.inputrc >.inputrc 2>/dev/null
